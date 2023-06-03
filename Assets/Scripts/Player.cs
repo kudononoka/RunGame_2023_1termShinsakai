f5b29343e7c8ee892fd4470e42246a6991fb593f
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-06-03 18:33:39 +0900

    [update]Player横移動完了

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody _rb;
+    [SerializeField,Header("前に進むスピード")] float _forwardMoveSpeed;
+    [SerializeField, Header("横移動スピード")] float _xMoveSpeed;
+    float _xMoveDir;
+    float _xMoveCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _xMoveDir = 0;
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && _xMoveCount > -1)
+        {
+            _xMoveDir = -1;
+            _xMoveCount--;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && _xMoveCount < 1)
+        {
+            _xMoveDir = 1;
+            _xMoveCount++;
+        }
+        _rb.AddForce(Vector3.right * _xMoveDir * _xMoveSpeed, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = Vector3.forward * _forwardMoveSpeed;
     }
 }
